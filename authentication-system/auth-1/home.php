79c5396958131e47b433e94b2a77ac015c57ba0e
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-12 20:03:21 +0600

    Update

---
+++ authentication-system/auth-1/home.php
@@ -0,0 +1,42 @@
+<?php 
+if(isset($_POST['logIn'])){
+    $username = $_POST['UN'];
+    $password = $_POST['pass'];
+    $txtfile = file("pass.txt");
+    $authenticated = false;
+
+    foreach($txtfile as $list){
+        list($_UN,$_Pass) = explode(",", $list);
+        if($_UN == $username && $_Pass == $password){
+            $authenticated = true;
+            break;
+        }
+    }
+    if($authenticated){
+        header("Location: first-page.php");
+    }else{
+        echo "<script>alert('Username or password is incorrect! Try Again.')</script>";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <div> 
+        <label for="UN">Username: </label><br>
+        <input type="text" name="UN"><br><br>
+        <label for="pass">Password: </label><br>
+        <input type="password" name="pass"><br><br>
+        <input type="submit" name="logIn" value="Log in">
+        </div>
+    </form>
+</body>
+</html>
