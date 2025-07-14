6eaa49aef519c009df50d592517eca477310915e
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-14 09:55:00 +0600

    New

---
+++ authentication-system/auth-2/first-page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div> 
+        <h2>Hi, Your password is 100% correct!</h2> <br> 
+        <h4>You are our original member. Thanks for comming back. Now enjoy all the contents in this page as much as you can!</h4>
+    </div>
+</body>
+</html>
---
+++ authentication-system/auth-2/login.php
@@ -0,0 +1,52 @@
+<?php 
+session_start(); //New
+// ob_start(); 
+// (to avoid header issues) যদি header() এর আগে কিছু output চলে আসে (যেমন whitespace, echo), তাহলে কাজ করবে না।  তাই রিস্ক এড়ানোর জন্য এই ফাংশন ব্যবহার করেছি। 
+// This is login page
+
+if(isset($_POST['logIn'])){
+    $username = $_POST['UN'];
+    $password = $_POST['pass'];
+    $txtfile = file("pass.txt");
+    $authenticated = false;
+
+    foreach($txtfile as $list){
+        list($_UN,$_Pass) = explode(",", $list);
+        if($_UN == $username && $_Pass == $password){
+            $authenticated = true;
+            break;
+        }
+    }
+    if($authenticated){
+        $_SESSION['start'] = $username; //New
+        header("Location: first-page.php");
+    }else{
+        // echo "<script>alert('Username or password is incorrect! Try Again.')</script>";
+        $message = "Username or password is incorrect! Try again.";
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo isset($message)? $message : '';
+    ?>
+    <form action="#" method="post">
+        <div> 
+        <label for="UN">Username: </label><br>
+        <input type="text" name="UN"><br><br>
+        <label for="pass">Password: </label><br>
+        <input type="password" name="pass"><br><br>
+        <input type="submit" name="logIn" value="Log in">
+        </div>
+    </form>
+</body>
+</html>
