67fcd85f9f0e5d08cb04ecfd8a500c6f86205bdf
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-15 10:07:12 +0600

    Update

--- file-upload/basic/step5.php
+++ file-upload/basic/step5.php
@@ -1,53 +1,50 @@
-
-
 <?php  
- if(isset($_POST['btnSubmit'])){ 
-    $filename=$_FILES['my_file']['name'];
-    $temp_file=$_FILES['my_file']['tmp_name'];
-    $file_size=$_FILES['my_file']['size'];
-    $file_type=$_FILES['my_file']['type'];
-    $img="img/";
-    $uploadOk = 1;
-    $kb=$file_size/1024;
-    
-  if($kb>400){ 
-  echo "File is too large.";
-  } else { 
-    move_uploaded_file("$temp_file","$img.$filename");
-    echo "Successfully!";
-  }
+$img = "img/";
+$filename = "";
+$show_preview = false;
 
-    if($file_type != "jpg" && $file_type != "jpeg"
-        && $file_type != "gif" ) {
-        echo " Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
+if(isset($_POST['btnSubmit'])) { 
+    $filename = $_FILES['my_file']['name'];
+    $temp_file = $_FILES['my_file']['tmp_name'];
+    $file_size = $_FILES['my_file']['size'];
+    $file_type = $_FILES['my_file']['type'];
+    $kb = $file_size / 1024;
+
+    $allowed_types = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+
+    if($kb > 400){ 
+        echo "❌ File is too large.";
+    } 
+    elseif (!in_array($file_type, $allowed_types)) {
+        echo "❌ Only JPG, JPEG, PNG & GIF files are allowed.";
+    } 
+    else {
+        if (move_uploaded_file($temp_file, $img . $filename)) {
+            echo "✅ Successfully uploaded!";
+            $show_preview = true;
+        } else {
+            echo "❌ Upload failed!";
+        }
     }
-   
-   
- }
-
+}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <form action="" method="post" enctype="multipart/form-data"> 
-      <input type="file" name="my_file">
+      <input type="file" name="my_file" required>
       <input type="submit" name="btnSubmit" value="upload">
     </form>
 
-    
+    <?php  
+    if($show_preview){
+        echo "<img src='" . $img . $filename . "' width='300px'>";
+    }
+    ?>
 </body>
 </html>
-
-<?php  
-
-	echo "<image src='$img.$filename' width='300px'>";
-
-?>
