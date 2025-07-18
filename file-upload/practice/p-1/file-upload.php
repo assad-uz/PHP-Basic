700cb5385b321e3afd9141e958ffe6828656fa05
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-19 01:41:05 +0600

    Update

--- file-upload/practice/p-1/file-upload.php
+++ file-upload/practice/p-1/file-upload.php
@@ -1,6 +1,14 @@
 <?php
-if(isset($_POST['up1'])){
-    
+$img = "img\";
+$filename = "";
+
+
+if(isset($_POST['btnSub'])){
+    $filename = $_FILES['btnSub']['name'];
+    $file_type = $_FILES['btnSub']['type'];
+    $temp_file = $_FILES['btnSub']['temp_name'];
+    $file_size = $_FILES['btnSub']['size'];
+
 }
 ?>
 
@@ -16,7 +24,8 @@ if(isset($_POST['up1'])){
 <div>
     <form action="#" method="post" enctype="multipart/form-data">
         <label for="up1">JPG file upload here: </label>
-        <input type="file" name="up1">
+        <input type="file" name="up1"><br><br>
+        <input type="submit" value="Upload" name="btnSub">
     </form>
 </div>
 </body>
