c2ca41f3640a29cf92fd0d7923761a69f1c1900f
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-19 00:42:22 +0600

    Update

--- file-upload/practice/p-1/file-upload.php
+++ file-upload/practice/p-1/file-upload.php
@@ -1,3 +1,9 @@
+<?php
+if(isset($_POST['up1'])){
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +14,7 @@
 <body>
 
 <div>
-    <form action="#" method="post" >
+    <form action="#" method="post" enctype="multipart/form-data">
         <label for="up1">JPG file upload here: </label>
         <input type="file" name="up1">
     </form>
