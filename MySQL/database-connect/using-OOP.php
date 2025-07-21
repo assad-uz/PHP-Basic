68f5b39d175eac314dbb3bbf760d793c0231e138
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-21 11:33:10 +0600

    Update

--- MySQL/database-connect/using-OOP.php
+++ MySQL/database-connect/using-OOP.php
@@ -10,3 +10,27 @@ if($conn->connect_error){
 }
 echo "connection successful";
 ?>
+<div> 
+    <h3 style="text-align: center;">Car Information</h3>
+    <table border="1" style="width: 50%; border-collapse: collapse; margin: 20px auto; font-family: Arial, sans-serif;"> 
+        <tr> 
+        <th>ID</th>
+        <th>Brand</th>
+        <th>Model</th>
+        <th>CC</th>
+        </tr>
+    
+    <?php 
+    $users = $conn->query("select * from users");
+    while (list($_id, $_brand, $_model,$_cc) = $users->fetch_row()){
+        echo "
+        <tr>
+        <td>$_id</td>
+        <td>$_brand</td>
+        <td>$_model</td>
+        <td>$_cc</td>
+        </tr>";
+    }
+    ?>
+    </table>
+</div>
