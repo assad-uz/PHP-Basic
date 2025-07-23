4f9359dea46216bd9b6c7785433731e41394967d
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-23 10:34:44 +0600

    New

--- MySQL/insert database/insert.php
+++ MySQL/insert database/insert.php
@@ -1,5 +1,11 @@
 <?php
 $db = mysqli_connect('localhost','root','my-db');
+    if(isset($_POST['submit'])){
+        $n = $_POST['txtName']
+        $a = $_POST['numAge']
+        $e = $_POST['txtEmail']
+        $c = $_POST['numContact']
+    }
 
 ?>
 <!DOCTYPE html>
@@ -8,8 +14,29 @@ $db = mysqli_connect('localhost','root','my-db');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    
+   <div class="container"> 
+    <div class="row"> 
+        <div class="col-sm-3"></div>
+        <a href="view.php">View Result</a>
+        <div class="col-sm-6 pt-4 mt-4 border border-success"> 
+    <form action="insert.php" method="POST"> 
+        Name:<br>
+        <input type ="text" name ="txtName"><br><br>
+        Age:<br>
+        <input type ="number" name ="numAge"><br><br>
+        Email:<br>
+        <input type ="email" name ="txtEmail"><br><br>
+        Contact:<br>
+        <input type ="number" name ="numContact"><br><br>
+        <input type ="submit" name ="submit" value="Insert" class="btn btn-success">
+    </form>
+        </div>
+        <!-- <div class="col-sm-3"></div> -->
+    </div>
+   </div>
+
 </body>
 </html>
