66bb35c971434e84b90140ee2a2efad0f43d3640
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-25 18:20:46 +0600

    Update

--- MySQL/connection-check.php
+++ MySQL/connection-check.php
@@ -1,5 +1,11 @@
 <?php
 // PHP-MySQL Connection Check
+$n = "Assaduzzaman";
+$e = "assad@example.com";
+$c = "01515-255259";
+
+$connt= mysqli_connect("localhost","root","","e_commerce");
+$sql= "INSERT INTO users (name,email,contact) VALUES ('$n','$e','$c')";
 
 if (mysqli_query($connt, $sql)) {
     echo "Query is running successfully.";
