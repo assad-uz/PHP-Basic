a9f078568060455205fa802f6de9d52f59ee924c
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-07-31 04:06:24 +0600

    Update

--- MySQL/exam/mid-term/evidence/insert-view-store-procedure/product-insert-delete-view.php
+++ MySQL/exam/mid-term/evidence/insert-view-store-procedure/product-insert-delete-view.php
@@ -1,35 +1,32 @@
+<!-- insert_manufacturer -->
 <?php 
 $connt= mysqli_connect("localhost","root","","manufacture_company");
-
+// add products
 if(isset($_POST['submit'])){
     $n = $_POST['name'];
-    $a = $_POST['address'];
-    $c = $_POST['contact_no'];
-
-    // Stored Procedure Call
-    $store = $connt->query("call call_insert_m('$n','$a','$c')");
-    if($store){
-        echo "Data inserted";
-        header('location: insert-product.php');
-        exit;
-    }else{
-        echo "Data not inserted" . $connt->error; 
-    }
+    $p = $_POST['price'];
+    $m_id = $_POST['manufacture_id'];
+    $store = $connt->query("call call_insert_p('$n','$p','$m_id')");
+}
+// delete manufacturer with products
+if(isset($_POST['delmanufact'])){
+	$mid = $_POST['manufac'];
+	$db->query(" delete from manufacturer where id='$mid ' ");
 }
 ?>
 
+<!-- products form and views-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <!-- <title>Medium Size Bootstrap Form</title> -->
   <title>Input Form</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
   <div class="container d-flex justify-content-center align-items-center vh-100">
     <div class="card shadow p-4" style="width: 100%; max-width: 500px;">
-      <h3 class="mb-4 text-center">Manufacturer Contact Form</h3>
+      <h3 class="mb-4 text-center">Product Information</h3>
       <form action="#" method="POST">
         <div class="mb-3">
           <label for="name" class="form-label">Name:</label>
@@ -37,13 +34,13 @@ if(isset($_POST['submit'])){
         </div> 
 
         <div class="mb-3">
-          <label for="address" class="form-label">Address:</label>
-          <input type="text" class="form-control" id="address" name="address" required>
+          <label for="price" class="form-label">Price:</label>
+          <input type="text" class="form-control" id="price" name="price" required>
         </div>
 
         <div class="mb-3">
-          <label for="contact_no" class="form-label">Contact No:</label>
-          <input type="text" class="form-control" id="contact_no" name="contact_no" required>
+          <label for="manufacture_id" class="form-label">manufacture_id:</label>
+          <input type="text" class="form-control" id="manufacture_id" name="manufacture_id" required>
         </div>
         
         <button type="submit" class="btn btn-primary w-100" name="submit">Submit</button>
